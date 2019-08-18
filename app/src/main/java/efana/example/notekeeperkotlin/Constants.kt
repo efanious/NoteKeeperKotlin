f737fe6a7501de265ca3b22fdc2ea008951ea5de
Efana Usua <usuaefana@gmail.com>
2019-08-18 22:31:58 +0100

    first commit

---
+++ app/src/main/java/efana/example/notekeeperkotlin/Constants.kt
@@ -0,0 +1,4 @@
+package efana.example.notekeeperkotlin
+
+const val NOTE_POSITION = "NOTE_POSITION"
+const val POSITION_NOT_SET = -1
---
+++ app/src/main/java/efana/example/notekeeperkotlin/NoteListActivity.kt
@@ -0,0 +1,36 @@
+package efana.example.notekeeperkotlin
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_note_list.*
+import kotlinx.android.synthetic.main.content_note_list.*
+
+class NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            val activityIntent = Intent(this, NoteActivity::class.java)
+            startActivity(activityIntent)
+        }
+
+        fab.setOnClickListener {
+            startActivity(Intent(this, NoteActivity::class.java))
+        }
+
+        listNotes.adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataManager.notes)
+
+        listNotes.setOnItemClickListener{parent, view, position, id ->
+            val activityIntent = Intent(this, NoteActivity::class.java)
+            activityIntent.putExtra(NOTE_POSITION, position)
+            startActivity(activityIntent)
+        }
+    }
+}
